ae18af876d36f65ddba1d5407d43b356187a4a43
박진훈 <hun9003@naver.com>
2022-02-04 17:59:59 +0900

    domain setting

---
+++ src/main/java/com/muesli/music/common/exception/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.muesli.music.common.exception;
+
+
+import com.muesli.music.common.response.ErrorCode;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(message, ErrorCode.COMMON_INVALID_PARAMETER);
+    }
+}
---
+++ src/main/java/com/muesli/music/common/exception/IllegalStatusException.java
@@ -0,0 +1,15 @@
+package com.muesli.music.common.exception;
+
+
+import com.muesli.music.common.response.ErrorCode;
+
+public class IllegalStatusException extends BaseException {
+
+    public IllegalStatusException() {
+        super(ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+
+    public IllegalStatusException(String message) {
+        super(message, ErrorCode.COMMON_ILLEGAL_STATUS);
+    }
+}

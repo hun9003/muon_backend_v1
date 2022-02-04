eac6bfbcdd4a50b67b1b9ce4a5c30b8eda1eb485
박진훈 <hun9003@naver.com>
2022-02-04 17:29:56 +0900

    first commit

---
+++ src/test/java/com/muesli/music/MuonBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muesli.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuonBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

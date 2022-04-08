052427b0647dd564edd2a2c342b6587b0eeacc2f
박진훈 <hun9003@naver.com>
2022-04-08 14:05:13 +0900

    스프링 부트 버전 2.6.3 -> 2.6.6

--- src/main/java/com/muesli/music/MuonBackendV1Application.java
+++ src/main/java/com/muesli/music/MuonBackendV1Application.java
@@ -8,6 +8,10 @@ public class MuonBackendV1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MuonBackendV1Application.class, args);
+        String getVersion = org.springframework.core.SpringVersion.getVersion();
+        String getVersion2 = org.springframework.boot.SpringBootVersion.getVersion();
+        System.out.println("스프링 프레임워크 버전 : " + getVersion);
+        System.out.println("스프링 부트 버전 : " + getVersion2);
     }
 
 }

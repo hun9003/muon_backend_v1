dcba669d1fdac8abf2219ab8041dad3d515e53a9
박진훈 <hun9003@naver.com>
2022-04-14 17:49:40 +0900

    가사(Lyrics) 테이블 구조 수정

--- src/main/java/com/muesli/music/domain/track/lyrics/Lyrics.java
+++ src/main/java/com/muesli/music/domain/track/lyrics/Lyrics.java
@@ -15,16 +15,29 @@ public class Lyrics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "timeline")
+    private String timaline;
+
     @Column(name = "text")
     private String text;
+
+    @Column(name = "text_original")
+    private String textOriginal;
+
+    @Column(name = "text_pron")
+    private String textPron;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "track_id")
     private Track track;
 
-    public Lyrics(Long id, String text, Track track) {
+    public Lyrics(Long id, String timaline, String text, String textOriginal, String textPron, Track track) {
         this.id = id;
+        this.timaline = timaline;
         this.text = text;
+        this.textOriginal = textOriginal;
+        this.textPron = textPron;
         this.track = track;
     }
 }

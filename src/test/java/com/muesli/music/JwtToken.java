f56ed4a239173728d6e537755e6d6d51161f8db8
hun9003 <hun9003@naver.com>
2022-07-17 17:57:18 +0900

    Spring RestDocs 공용 토큰 작성

---
+++ src/test/java/com/muesli/music/JwtToken.java
@@ -0,0 +1,6 @@
+package com.muesli.music;
+
+public class JwtToken {
+    public static final String JWT_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJodW45MDAzQG5hdmVyLmNvbSIsImV4cCI6MTY1ODY0NjgzMn0.Kdweujj696PJQmgbGUrgbD1psIe6wLTcQVQecEPCAVc";
+
+}
